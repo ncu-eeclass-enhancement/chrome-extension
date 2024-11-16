c683252bd08b1812cad83b97d1e33ef50b301dac
boyiliu1007 <119803346+boyiliu1007@users.noreply.github.com>
2024-11-16 22:00:06 +0800

    init vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="frame p-0 m-0 bg-black flex justify-center items-center">
+    <div class = "h-full w-full p-0 m-0 flex "></div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.frame {
+  width: 80vw;
+  height: 90vh;
+}
+</style>
